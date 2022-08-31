2a14415a90af6cf2c11c6f8caf307d8e74554e64
dharmveer47 <singhdharmu89@gmail.com>
2022-08-31 15:50:04 +0530

    added pop up

--- src/components/posComponents/PosCustomeSelection.js
+++ src/components/posComponents/PosCustomeSelection.js
@@ -9,21 +9,38 @@ import { RiAddCircleFill, RiArrowDropDownLine } from "react-icons/ri";
 import "../../Styles/PosStyle/PosCustomer.scss";
 const PosCustomeSelection = () => {
   const [toggle, setToggle] = useState(true);
+  const [addressToggle, setAddressToggle] = useState(false);
   return (
     <PosThem className="pos__customer_selection">
-      <SingelChooseCustomer setToggle={setToggle} cus="Choose a Customer" />
-      <SingelChooseCustomer cus="Choose a Customer Address" />
+      <SingelChooseCustomer
+        onClick={() => {
+          setToggle((e) => !e);
+        }}
+        cus="Choose a Customer"
+      />
+
+      <SingelChooseCustomer
+        onClick={() => {
+          setAddressToggle((e) => !e);
+        }}
+        cus="Choose a Customer Address"
+      />
+
       <PosPopUp setToggle={setToggle} toggle={toggle} />
+      <PosPopUpAddress
+        setAddressToggle={setAddressToggle}
+        addressToggle={addressToggle}
+      />
     </PosThem>
   );
 };
 
-const SingelChooseCustomer = ({ cus, setToggle }) => {
+const SingelChooseCustomer = ({ cus, onClick }) => {
   return (
     <div className="pos__singleCustomer">
       <div className="pos__choose_customer">
         <h1 className="pos__choose_heading">{cus}</h1>
-        <RiAddCircleFill onClick={() => setToggle((e) => !e)} />
+        <RiAddCircleFill onClick={onClick} />
       </div>
       <div className="pos__choose_input">
         <input type="text" value="Choose Customer" />
@@ -54,6 +71,47 @@ const PosPopUp = ({ toggle, setToggle }) => {
       </div>
       <div className="pos__customer_button">
         <Button bName="Add Customer" />
+      </div>
+    </PosThem>
+  );
+};
+
+const PosPopUpAddress = ({ setAddressToggle, addressToggle }) => {
+  if (addressToggle) {
+    return <></>;
+  }
+  return (
+    <PosThem className="pos__pop_up_customer">
+      <div className="pos__pop_up_headings">
+        <Heading des="Add Customer Address" />
+        <MdClose onClick={() => setAddressToggle((e) => !e)} />
+      </div>
+      <LineBrek />
+      <div className="pos__input_customer_selection">
+        <CustomerAddInput name="First Name" placeholder="Enter First Name" />
+        <CustomerAddInput name="Last Name" placeholder="Enter Last Name" />
+      </div>
+      <div className="pos__input_customer_selection">
+        <CustomerAddInput name="Postal Code" placeholder="Enter Postal Code" />
+        <CustomerAddInput name="City" placeholder="Enter City" />
+      </div>
+      <div className="pos__input_customer_selection">
+        <CustomerAddInput name="Country" placeholder="Enter Country" />
+        <CustomerAddInput name="State" placeholder="Enter State" />
+      </div>
+      <div className="pos__input_customer_selection">
+        <CustomerAddInput
+          name="Street Address"
+          placeholder="Enter Street Address"
+        />
+        <CustomerAddInput name="Company" placeholder="Enter Company" />
+      </div>
+      <div className="pos__input_customer_selection">
+        <CustomerAddInput name="Lat Lng" placeholder="Enter Lat Lng" />
+        <CustomerAddInput name="Contact" placeholder="Enter Contact" />
+      </div>
+      <div className="pos__customer_button">
+        <Button bName="Add Customer Address" />
       </div>
     </PosThem>
   );
